cc19e8e5786c691258cbf94d77014b03be606fb2
Grillon <thierry.grillon@gmail.com>
2024-09-28 21:42:55 +0200

    activation form tp/sl

--- callbacks/popup.py
+++ callbacks/popup.py
@@ -6,12 +6,16 @@ from app_instance import app
 @app.callback(
     Output("row-selection-modal", "is_open"),
     Output("row-selection-modal-content", "children"),
+    Output("tp","value"),
+    Output("sl","value"),
+    Output("ppmax","value"),
+    Output("capital","value"),
     Input("ag-grid", "selectedRows"),
     Input("row-selection-modal-close", "n_clicks"),
 )
 def open_modal(selection, _):
     if ctx.triggered_id == "row-selection-modal-close":
-        return False, no_update
+        return False, no_update, no_update, no_update, no_update, no_update
     if selection:
         return True, "You selected " + ", ".join(
             [
@@ -19,6 +23,28 @@ def open_modal(selection, _):
                 f"for a total of {s['usd_balance']}"
                 for s in selection
             ]
-        )
+        ),selection[0]['tp'],selection[0]['sl'],selection[0]['ppmax'],selection[0]['capital']
 
-    return no_update, no_update
+    return no_update, no_update, no_update, no_update, no_update, no_update
+
+@app.callback(
+    Output("ag-grid","rowData"),
+    Input("row-selection-modal-close", "n_clicks"),
+    State("ag-grid","selectedRows"),
+    State("tp","value"),
+    State("sl","value"),
+    State("ppmax","value"),
+    State("capital","value"),
+    State("ag-grid","rowData"),
+)
+def update_tableau(on_click,selectedRows,tp,sl,ppmax,capital,rowData):
+    if selectedRows and on_click:
+        row_to_update = selectedRows[0]
+        for row in rowData:
+            if row == row_to_update:
+                row['tp'] = tp
+                row['sl'] = sl
+                row['ppmax'] = ppmax
+                row['capital'] = capital
+        return rowData
+    return rowData
